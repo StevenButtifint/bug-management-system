423f213ec0ae039c7d828038ebd59cde0526690a
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-01 20:17:46 +0100

    updated app starting page logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 import Login from './Views/Login/login'
-
+import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './Views/Sidebar/sidebar'
+import ViewBugPage from './Views/Pages/viewBugs'
 
 function App() {
   const {auth} = useSelector(state => state)/*are we logged in*/
   return (
-    <>
-    {!auth.LoggenIn ? <Login />:
-    <></>
-    }
-    </>
+    <Router>
+      {!auth.LoggenIn ? <Login />:
+        <>
+          <Sidebar />
+          <ViewBugPage />
+        </>
+      }
+    </Router>
   );
 }
 
