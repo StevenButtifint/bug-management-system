7b613f813bbb43bece26515adfb697f73b5da098
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-13 23:19:59 +0100

    added dashboard card

---
+++ frontend/src/Views/Components/Dashboard/card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Priority from '../../../Controllers/priorityController'
+
+import './card.css'
+
+export default (props)=>{
+    const {level,color} = Priority(props.Priority);
+
+
+    return(
+        <div className='dashboard-card' onClick={props.clicked} style={{color: color}}>
+            <h2>Total: {level}</h2>
+            <p>{props.count}</p>
+        </div>
+    )
+}
