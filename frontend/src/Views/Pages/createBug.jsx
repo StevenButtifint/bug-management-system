705076dfb8de82f5a28564eb80c9a3c0d3092714
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-20 15:41:34 +0100

    added createBug page

---
+++ frontend/src/Views/Pages/createBug.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import CreateBug from '../../Views/Components/Bug Create & Edit/bugForm'
+
+
+
+export default()=>{
+    return(
+        <div className='pages-BG'>
+            <div className='page-container'>
+                <CreateBug title="Create Bug" />
+            </div>
+        </div>
+    )
+}
