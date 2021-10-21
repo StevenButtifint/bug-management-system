767ddab5a2261fb51db019496ba4438ade5c505d
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-22 00:03:00 +0100

    added header to card

--- frontend/src/Views/Components/Dashboard/card.jsx
+++ frontend/src/Views/Components/Dashboard/card.jsx
@@ -4,12 +4,13 @@ import Priority from '../../../Controllers/priorityController'
 import './card.css'
 
 export default (props)=>{
-    const {level,color} = Priority(props.Priority);
+    const {level,color} = Priority(props.priority);
 
 
     return(
         <div className='dashboard-card' onClick={props.clicked} style={{color: color}}>
-            <h2>Total: {level}</h2>
+            <h2>{level}</h2>
+            <h3> Priority Bugs </h3>
             <p>{props.count}</p>
         </div>
     )
