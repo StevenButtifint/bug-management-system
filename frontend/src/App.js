e031cea79559fe10fc54bd245eecb3b3fe43c9cf
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-23 21:02:49 +0100

    organised router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
 import {useSelector} from 'react-redux'
-import Login from './Views/Login/login'
-import {BrowserRouter as Router} from 'react-router-dom'
+import Login from './Views/Pages/Login/login'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Sidebar from './Views/Sidebar/sidebar'
 import ViewBugPage from './Views/Pages/viewBugs'
+import CreateBugPage from './Views/Pages/createBug'
+import CreateBug from './Views/Components/Bug Create & Edit/bugForm'
+import Dashboard from './Views/Pages/Dashboard/dashboard'
 
 function App() {
   const {auth} = useSelector(state => state)/*are we logged in*/
   return (
     <Router>
-      {!auth.LoggenIn ? <Login />:
+      {!auth.LoggedIn ? <Login />:
         <>
           <Sidebar />
-          <ViewBugPage />
+          <Switch>
+          <Route path="/" exact ><Dashboard /></Route>
+            <Route path="/viewbugs"><ViewBugPage /></Route>
+            <Route path="/create"><CreateBugPage /></Route>
+          </Switch>
         </>
       }
     </Router>
